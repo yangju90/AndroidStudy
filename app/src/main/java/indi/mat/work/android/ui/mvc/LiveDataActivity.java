5dce51a03299dad032429d713ecf5bd3d575ec52
Mat <yangju90@163.com>
2021-10-11 23:23:44 +0800

    Update LiveDataActivity.java

--- app/src/main/java/indi/mat/work/android/ui/mvc/LiveDataActivity.java
+++ app/src/main/java/indi/mat/work/android/ui/mvc/LiveDataActivity.java
@@ -27,6 +27,7 @@ public class LiveDataActivity extends BaseActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_live_data);
         liveDataViewModel = new ViewModelProvider(this).get(LiveDataViewModel.class);
         liveDataViewModel = new ViewModelProvider(this, new SavedStateVMFactory(this)).get(LiveDataViewModel.class);
+        
 
         Log.d(TAG, "onCreate: " + liveDataViewModel.getLikedNumber());
 
