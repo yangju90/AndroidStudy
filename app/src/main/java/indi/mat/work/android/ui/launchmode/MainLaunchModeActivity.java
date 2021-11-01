b7da6463cc96ed497ce78d6b497da194838b8e13
yangju90 <yangju90@163.com>
2021-11-01 17:25:07 +0800

    修改

---
+++ app/src/main/java/indi/mat/work/android/ui/launchmode/MainLaunchModeActivity.java
@@ -0,0 +1,15 @@
+package indi.mat.work.android.ui.launchmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import indi.mat.work.android.R;
+
+public class MainLaunchModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_launch_mode);
+    }
+}
